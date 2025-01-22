1cb647ba25d994192473e051c43ac65cdf94fb4f
kayqueFranco <kayque.franco2018@gmail.com>
2025-01-22 17:19:47 -0300

    Atualização da aula 2

---
+++ fundamentos/operadores.js
@@ -0,0 +1,37 @@
+/**
+ * OPERADORES
+ * - Aritméticos
+ * - Aritméticos de atribuição
+ * - comparação
+ * - lógicos
+ */
+console.clear()
+console.log("Operadores aeitméticos")
+console.log("+|soma")
+console.log("-|subtração")
+console.log("*|multiplicação")
+console.log("/|divisão")
+console.log("%|módulo (Resto da divisão)")
+console.log("--------------------------------------")
+console.log("Operadores aeitméticos de atribuição")
+console.log("+=|soma e atribuição variavel")
+console.log("-=|subtração e atribuição variavel")
+console.log("*=|multiplicação e atribuição variavel")
+console.log("/=|divisão e atribuição variavel")
+console.log("++| somar 1 ao resultado ")
+console.log("--| subtrair 1 do resultado")
+console.log("----------------------------------------------")
+console.log("Comparadores")
+console.log(">  | maior que ")
+console.log("<  | menor que ")
+console.log(">= | maior iqual a ")
+console.log("<= | menor igual a ")
+console.log("===| indêntico a  ")
+console.log("!==| indêntico a ")
+console.log("-------------------------------------------------")
+console.log("Operadores logicos")
+
+console.log("&&|AND")
+console.log("|||OR")
+console.log("! |NOT")
+
